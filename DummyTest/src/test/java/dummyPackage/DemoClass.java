5b1721e8a26c9ecada65033616231cac82ce0b5e
aaggarwalPayfare <aaggarwal@payfare.com>
2022-04-06 01:04:48 +0530

    Add a test

---
+++ DummyTest/src/test/java/dummyPackage/DemoClass.java
@@ -0,0 +1,11 @@
+package dummyPackage;
+
+import org.testng.annotations.Test;
+
+public class DemoClass {
+
+    @Test
+    public void test1() {
+        System.out.println(System.getProperty("app"));
+    }
+}
